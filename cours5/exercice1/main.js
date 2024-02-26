125d32d8d1bef0b08442f4796dff5166a23f7326
Tommy Gagnon Joyal <tommygjoyal@gmail.com>
2024-02-25 21:35:19 -0500

    Add Cours 5

--- cours5/exercice1/main.js
+++ cours5/exercice1/main.js
@@ -1,14 +1,38 @@
 // exercice 1
 
 // egal
+console.log(5 == 5);
+
 // different de 
+console.log(5 != 5); 
+
 // plus grand que
+console.log(5 > 5);
+
 // plus grand ou egal que
+console.log(5 >= 5);
+
 // plus petit que
-// plus petit ou egal que 
+console.log(5 < 5);
+
+// plus petit ou egal que
+console.log(5 <= 5); 
 
 // meme type et egal ( 5 et 5)
+console.log(5 === 5); 
+
 // meme type et egal ( 5 et "5")
+console.log(5 === "5"); 
+
 // type different ou inegal ( 5 et 5)
+console.log(5 !== 5);
+
 // type different ou inegal ( 5 et "5")
+console.log(5 !== "5");
+
+
+
+
+
+
 
